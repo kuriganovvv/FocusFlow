833f411dc786ba774352d9927a00933bb0076882
JSquirell <dusmatov.odil141126@gmail.com>
2025-11-12 01:31:15 +0700

    работа над task-item

--- src/main/java/tasks/TaskItem.java
+++ src/main/java/tasks/TaskItem.java
@@ -1,6 +1,18 @@
 package tasks;
+import java.time.LocalDate;
 
 public class TaskItem {
+    private String title;
+
+    private LocalDate date;
+
+    public TaskItem(String title, LocalDate date) {
+        this.title = title;
+        this.date = date;
+    }
+
+    public String getTitle() {return title;}
+    public LocalDate getDate() {return date;}
     /* название 
      * дата (дедлайн)
      * дней до дедлайна
