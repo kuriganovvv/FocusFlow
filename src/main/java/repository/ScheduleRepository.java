5e5bb3e5586c8ecfe19b76712b31ee6981e51c4c
VladKuriganov <vlad20033010@gmail.com>
2025-12-03 19:02:47 +0700

    feat(SQL): Создан  ScheduleRepository класс

---
+++ src/main/java/repository/ScheduleRepository.java
@@ -0,0 +1,42 @@
+package repository;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+import model.ScheduleEntity;
+
+public class ScheduleRepository {
+    private EntityManagerFactory emf;//фабрика звезд
+    
+    public ScheduleRepository(){// Персистенс.хмл
+        emf = Persistence.createEntityManagerFactory("schedule_pu");
+    }
+    
+    public void save(ScheduleEntity entity){
+        EntityManager em = emf.createEntityManager();// Лариса Долина 
+        try { // продает квартиру 
+            em.getTransaction().begin(); // сделка началась
+            em.persist(entity); // Лариса получила деньги
+            em.getTransaction().commit(); // сделка закончилась
+        } finally{
+            em.close();// Долина вернет квартиру при любом исходе 
+        }
+    }
+    
+    public List<ScheduleEntity> findAll(){
+        EntityManager em = emf.createEntityManager();
+        try {
+            /*JPQL запрос  */
+            return em.createQuery(
+                "SELECT s FROM ScheduleEntity s", // выбрать такие s, что ЧедулЭнтити имеют тип 
+                ScheduleEntity.class // вернуть обьект
+            ).getResultList(); // вернуть List<обьект>
+        } finally{
+            em.close();// исход не важен, закрыть мы должны
+        }
+    }
+    /* Звезды кончились, но цирк открыт */
+    public void close() {
+        if (emf != null) emf.close();// закроем его
+    }
+}
