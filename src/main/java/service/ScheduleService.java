15b81d6ee972b078aa675a51a19d857f74a7a5cd
VladKuriganov <vlad20033010@gmail.com>
2025-12-03 19:03:33 +0700

    feat(sql):Обновлен сервис работы с расписанием для БД

--- src/main/java/service/ScheduleService.java
+++ src/main/java/service/ScheduleService.java
@@ -10,11 +10,37 @@ import model.Schedule;
 
 public class ScheduleService{
     private final Map<String, List<Schedule>>schedule=new LinkedHashMap<>();
+    private final ScheduleRepository repository;
+
+    public final String[] DAYS ={"Пн","Вт","Ср","Чт","Пт","Сб"};
 
     public ScheduleService(){
-        String[]days={"Пн","Вт","Ср","Чт","Пт","Сб"};
-        for(String day:days){
+        for(String day:DAYS){
             schedule.put(day,new ArrayList<>());
+        }
+        repository=new ScheduleRepository(); // сервис->репозиторий->ентити
+        loadFromDatabase();
+    }
+    private void loadFromDatabase(){
+        try{
+            /*Загрузка */
+            List<ScheduleEntity> entities =repository.findAll();
+            /*Очищение памяти */
+            for(String day:DAYS){
+                schedule.get(day).clear();
+            }
+            /*Заполнение */
+            for(ScheduleEntity entity:entities){
+                String day= entity.getDay();
+                String time= entity.getTime();
+                String subject= entity.getSubject();
+                /* Запись в память */
+                schedule.get(day).add(new Schedule(subject, time));
+                Collections.sort(schedule.get(day));
+            }
+            System.out.println("Загрузка завершена!");
+        }catch(Exception e){
+            System.out.println("Ошибка загрузки БД"+e.getMessage());
         }
     }
 
